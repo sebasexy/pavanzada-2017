b0103b1d429c618decd869eac988b5a018451b21
A01224439 <spider_cbaz_10@hotmail.com>
2017-10-12 21:01:59 -0500

    Kata4 completa

--- kata4/island-perimeter.c
+++ kata4/island-perimeter.c
@@ -2,6 +2,18 @@
 #include <stdio.h>
 #include "island-perimeter.h"
 
+/* islandPerimeter
+* From a matrix of integers, 0 - represents water, 1 - represents land
+* it calculates the perimeter of the island.
+* cells are connected horizontally/vertically (not diagonally)
+* @params:
+*     array - the matrix of integers
+*     rows - how many rows the matrix has
+      columns - how many columns the matrix has
+* @ return:
+*   int - what is the perimeter of the island
+*
+*/
 int islandPerimeter(int** array, int rows, int columns){
   int perimeter = 0;
   short neighbors;
@@ -16,7 +28,7 @@ int islandPerimeter(int** array, int rows, int columns){
         if((i-1) >= 0 && array[i-1][j] == 1){
           neighbors++;
         }
-        if(i+1 < columns && array[i+1][j] == 1){
+        if(i+1 < rows && array[i+1][j] == 1){
           neighbors++;
         }
         if(j-1 >= 0 && array[i][j-1] == 1){
