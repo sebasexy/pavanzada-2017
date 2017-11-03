eff003a4b146e2093a2a05323f52925c6352c3c2
A01224439 <spider_cbaz_10@hotmail.com>
2017-11-02 20:58:37 -0600

    Forks

--- threads/main.c
+++ threads/main.c
@@ -1,10 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
+#include <unistd.h>
+#include <semaphore.h>
 #include "threads.h"
-#define NUM_THREADS     50
+#define NUM_THREADS    10
 
 int unsigned total;
+sem_t sem1;
+sem_t forks[NUM_THREADS];
+
+void thinking(int n){
+  sleep(rand()%n);
+}
+void eating(int n){
+  sleep(rand()%n);
+}
+
+void *lifeStyle(void *threadid){
+  long tid;
+  tid = (long)threadid;
+  while(1){
+    thinking(3);
+    printf("I am phil %lu, I am hungry\n", tid);
+    sem_wait(&forks[tid]);
+    sem_wait(&forks[(tid+1)%NUM_THREADS]);
+    eating(4);
+    sem_post(&forks[tid]);
+    sem_post(&forks[(tid+1)%NUM_THREADS]);
+    printf("I am phil %lu, I am thinking\n", tid);
+  }
+
+}
 
 void *add100(void *threadid){
 
@@ -14,9 +41,12 @@ void *add100(void *threadid){
 
    unsigned int localTotal;
    printf("Adding 100 #%ld!\n", tid);
+   sem_wait(&sem1);
    localTotal = total;
    localTotal += 100;
+   sleep(tid%5);
    total = localTotal;
+   sem_post(&sem1);
 
    pthread_exit(NULL);
 }
@@ -26,10 +56,11 @@ int main (int argc, char *argv[])
    pthread_t threads[NUM_THREADS];
    int rc;
    long t;
-   total = 0;
+   //total = 0;
+   //sem_init(&sem1, 1, 1);
    for(t=0; t<NUM_THREADS; t++){
       printf("In main: creating thread %ld\n", t);
-      rc = pthread_create(&threads[t], NULL, add100, (void *)t);
+      rc = pthread_create(&threads[t], NULL, lifeStyle, (void *)t);
       if (rc){
          printf("ERROR; return code from pthread_create() is %d\n", rc);
          exit(-1);
@@ -40,6 +71,6 @@ int main (int argc, char *argv[])
      pthread_join(threads[t], NULL);
    }
 
-   printf("Final total =%d \n", total);
+   //printf("Final total =%d \n", total);
    /* Last thing that main() should do */
 }
