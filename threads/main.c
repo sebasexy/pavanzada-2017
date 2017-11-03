552e1d5e194b1d210617a41fcc4492068ad115bc
A01224439 <spider_cbaz_10@hotmail.com>
2017-11-02 19:48:28 -0600

    hehe

--- threads/main.c
+++ threads/main.c
@@ -6,13 +6,18 @@
 
 int unsigned total;
 
-void *add100(void *threadid)
+void *add100(void *threadid){
 
    long tid;
 
    tid = (long)threadid;
+
+   unsigned int localTotal;
    printf("Adding 100 #%ld!\n", tid);
-   total+=100;
+   localTotal = total;
+   localTotal += 100;
+   total = localTotal;
+
    pthread_exit(NULL);
 }
 
@@ -24,14 +29,17 @@ int main (int argc, char *argv[])
    total = 0;
    for(t=0; t<NUM_THREADS; t++){
       printf("In main: creating thread %ld\n", t);
-      rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
+      rc = pthread_create(&threads[t], NULL, add100, (void *)t);
       if (rc){
          printf("ERROR; return code from pthread_create() is %d\n", rc);
          exit(-1);
       }
    }
 
-|
+   for(t = 0; t < NUM_THREADS; t++){
+     pthread_join(threads[t], NULL);
+   }
+
+   printf("Final total =%d \n", total);
    /* Last thing that main() should do */
-   pthread_exit(NULL);
 }
